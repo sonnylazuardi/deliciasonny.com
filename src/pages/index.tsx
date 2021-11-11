767e29e13673f3cb78ed8c9d1e8b1ca695df2091
Ridho Akbarisanto <ridho.akbarisanto@bukalapak.com>
2021-11-12 00:01:50 +0700

    fix a lot of things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,50 +2,60 @@ import React from 'react'
 import tw from 'twin.macro'
 import { Logo, Layout, Button, Link } from '../components'
 import GroomsBride from '../images/groomsbride.png'
+import ShuangXi from '../images/shuangxi.png'
 import QRImage from '../images/qrocbc.png'
 import Paynow from '../images/paynow.jpg'
 import Googlepay from '../images/googlepay.jpg'
+import NameTagFitri from '../images/NameTag_Fitri.png'
+import NameTagRidho from '../images/NameTag_Ridho.png'
 const App = () => {
   const [showGiving, setShowGiving] = React.useState(false)
   return (
     <Layout css={tw`min-h-screen overflow-hidden relative`}>
-      <div tw="absolute -top-16 -right-16 w-48 h-48 transform rotate-180 z-10" />
       <div
         className="container"
         tw="mx-auto flex flex-col items-center relative"
       >
-        {/*<div tw="p-5 block mt-24">*/}
-          {/*<Logo />*/}
-        {/*</div>*/}
-        <div tw="font-mono text-center" style={{
-          right: 0,
-          bottom: '4rem',
-          marginTop: '10rem',
+        <div tw="font-serif font-bold text-center" style={{
+          marginTop: '5rem',
+          marginBottom: '5rem',
           fontSize: '1rem',
           color: "#2a3b72"
         }}>
           THE WEDDING OF
         </div>
-        <div tw="font-brittany text-center" style={{
+{/*        <div tw="p-5">
+          <img src={ShuangXi} />
+        </div>
+*/}        <div tw="w-3/5 p-5">
+          <img src={NameTagFitri} />
+        </div>
+{/*        <div tw="font-brittany text-center" style={{
+          fontWeight: 'bold',
           marginTop: '10rem',
-          fontSize: '6.5rem',
+          fontSize: '5rem',
           color:"#2a3b72"
         }}>
           Fitri Lin
-        </div>
+        </div>*/}        
         <div tw="font-brittany text-center" style={{
-          fontSize: '2rem',
+          fontSize: '4rem',
           color:"#2a3b72"
         }}>
           &
         </div>
-        <div tw="font-brittany text-center" style={{
+        <div tw="w-full p-5" style={{
+          marginBottom: '10rem'
+        }}>
+          <img src={NameTagRidho} />
+        </div>
+{/*        <div tw="font-brittany text-center" style={{
           marginBottom: '10rem',
-          fontSize: '6.5rem',
+          fontSize: '5rem',
           color:"#2a3b72"
         }}>
           Ridho Akbarisanto
-        </div>
+        </div>*/}
       </div>
       <div tw="relative bg-white mx-auto flex flex-col items-center relative">
         <div tw="w-full">
@@ -63,41 +73,40 @@ const App = () => {
           <div tw="text-center">
             Tanpa mengurangi rasa hormat dan untuk dapat menerapkan protokol kesehatan secara maksimal, maka kami melakukan pembatasan terhadap tamu yang hadir pada acara ini. Oleh karena itu, kami memohon doa restu dan mengundang bapak/ibu dan teman-teman sekalian untuk dapat menyaksikan prosesi pernikahan kami secara live streaming melalui link di bawah ini:
           </div>
-          <div tw="grid grid-cols-2 gap-4 text-center font-poppin py-20">
-            <div tw="col-span-full sm:col-span-1 flex flex-col items-center justify-center text-3xl tracking-widest sm:mb-0 mb-12 font-bold">
+          <div tw="grid grid-cols-2 gap-4 text-center font-poppin py-10">
+            <div tw="col-span-full sm:col-span-1 flex flex-col items-center justify-center text-3xl tracking-widest font-bold">
               MINGGU
               <br />
               21
               <br />
               NOV
               <br />
-              21
+              2021
             </div>
             <div tw="col-span-full sm:col-span-1 text-2xl tracking-widest font-poppin">
-              <div tw="text-lg">Akad</div>
+              <div tw="text-lg font-bold">Akad</div>
               <div tw="text-lg">08.00 WIB</div>
               <br />
-              <div tw="text-lg">Intimate Reception</div>
+              <div tw="text-lg font-bold">Intimate Reception</div>
               <div tw="text-lg">10.00 WIB</div>
             </div>
           </div>
-          <div tw="grid grid-cols-3 gap-4 text-center font-poppin py-20">
-            <div tw="col-span-full sm:col-span-1 text-2xl tracking-widest font-poppin">
+          <div tw="grid grid-cols-3 gap-4 text-center font-poppin pt-10">
+            <div tw="col-span-full sm:col-span-1">
             </div>
-            <div tw="px-12 col-span-full sm:col-span-1 text-2xl tracking-widest font-poppin">
+            <div tw="col-span-full sm:col-span-1 text-2xl tracking-widest font-poppin">
               <div tw="flex mb-4">
                 <Link isPrimary={true} href="https://youtu.be/Ne-r6KDFWH4" style={{backgroundColor:"#2a3b72"}}>
                   Live Stream
                 </Link>
               </div>
             </div>
-            <div tw="col-span-full sm:col-span-1 text-2xl tracking-widest font-poppin">
+            <div tw="col-span-full sm:col-span-1">
             </div>
           </div>
         </div>
       </div>
       <div tw="py-24 relative bg-white">
-        <div tw="absolute -top-24 -right-16 w-48 h-48 transform rotate-180" />
         <div
           className="container"
           tw="mx-auto items-center pb-12 px-4 sm:px-0 relative"
